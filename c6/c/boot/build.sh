e9506a1e6946cebf23cfe5f433c46eb4becbba35
lihaifei <lihaifei@192.168.9.153>
2022-10-29 13:30:17 -0400

    modify build.sh

--- c6/c/boot/build.sh
+++ c6/c/boot/build.sh
@@ -1,8 +1,9 @@
 nasm -I include/ -o mbr.bin mbr.S
 dd if=./mbr.bin of=../../../bochs-2.6.2/hd60M.img bs=512 count=1  conv=notrunc
 nasm -I include/ -o loader.bin loader.S
-dd if=./loader.bin of=../../../bochs-2.6.2/hd60M.img bs=512 count=4 seek=2 conv=notrunc
+dd if=./loader.bin of=../../../bochs-2.6.2/hd60M.img bs=512 count=3 seek=2 conv=notrunc
+
 nasm -f elf -o ../lib/kernel/print.o ../lib/kernel/print.S
-gcc -I ../lib/kernel -c -o ../kernel/main.o ../kernel/main.c
-ld -Ttext 0xc0001500 -e main -o kernel.../bin kernel/main.o ../lib/kenel/print.o
-dd if=./kernel.bin of=./../../bochs-2.6.2/hd60M.img bs=512 count=200 seek=9 conv=notrunc
+gcc -I ../lib/kernel -m32 -c -o ../kernel/main.o ../kernel/main.c
+ld -melf_i386 -Ttext 0xc0001500 -e main -o kernel.bin ../kernel/main.o ../lib/kernel/print.o
+dd if=./kernel.bin of=../../../bochs-2.6.2/hd60M.img bs=512 count=200 seek=9 conv=notrunc
