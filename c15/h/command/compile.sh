87245cb6747e21d5329d8252eb5cbc966b40e128
lihaifei <mecuryhai@gmail.com>
2023-10-03 16:33:42 +0800

    c15 h 使用户进程支持参数 done

--- c15/h/command/compile.sh
+++ c15/h/command/compile.sh
@@ -7,21 +7,23 @@ if [[ ! -d "../lib" || ! -d "../build" ]];then
    cwd=${cwd%/}
    if [[ $cwd != "command" ]];then
       echo -e "you\`d better in command dir\n"
-   fi
+   fi 
    exit
 fi
 
-BIN="prog_no_arg"
+BIN="prog_arg"
 CFLAGS="-Wall -m32 -c -fno-builtin -W -Wstrict-prototypes \
       -Wmissing-prototypes -Wsystem-headers"
-LIB="../lib/"
+LIBS="-I ../lib -I ../lib/user -I ../fs"
 OBJS="../build/string.o ../build/syscall.o \
-      ../build/stdio.o ../build/assert.o"
+      ../build/stdio.o ../build/assert.o start.o"
 DD_IN=$BIN
 DD_OUT="../../../bochs-2.6.2/hd60M.img"
 
-gcc $CFLAGS -I $LIB -o $BIN".o" $BIN".c"
-ld -melf_i386 -e main $BIN".o" $OBJS -o $BIN
+nasm -f elf ./start.S -o ./start.o
+ar rcs simple_crt.a $OBJS start.o
+gcc $CFLAGS $LIBS -o $BIN".o" $BIN".c"
+ld -melf_i386 $BIN".o" simple_crt.a -o $BIN
 SEC_CNT=$(ls -l $BIN|awk '{printf("%d", ($5+511)/512)}')
 
 if [[ -f $BIN ]];then
@@ -29,10 +31,12 @@ if [[ -f $BIN ]];then
    count=$SEC_CNT seek=300 conv=notrunc
 fi
 
-##########   以上核心就是下面这三条命令   ##########
+##########   以上核心就是下面这五条命令   ##########
+#nasm -f elf ./start.S -o ./start.o
+#ar rcs simple_crt.a ../build/string.o ../build/syscall.o \
+#   ../build/stdio.o ../build/assert.o ./start.o
 #gcc -Wall -c -fno-builtin -W -Wstrict-prototypes -Wmissing-prototypes \
-#   -Wsystem-headers -I ../lib -o prog_no_arg.o prog_no_arg.c
-#ld -e main prog_no_arg.o ../build/string.o ../build/syscall.o\
-#   ../build/stdio.o ../build/assert.o -o prog_no_arg
-#dd if=prog_no_arg of=/home/work/my_workspace/bochs/hd60M.img \
-#   bs=512 count=10 seek=300 conv=notrunc
+#   -Wsystem-headers -I ../lib/ -I ../lib/user -I ../fs prog_arg.c -o prog_arg.o
+#ld prog_arg.o simple_crt.a -o prog_arg
+#dd if=prog_arg of=/home/work/my_workspace/bochs/hd60M.img \
+#   bs=512 count=11 seek=300 conv=notrunc
