224cd5eea992f340f2d1b378ca7ac98aa8cfddcd
lihaifei <mecuryhai@gmail.com>
2023-10-03 11:46:13 +0800

    c15 c done

--- c15/c/shell/shell.c
+++ c15/c/shell/shell.c
@@ -26,6 +26,7 @@ void print_prompt(void) {
 static void readline(char* buf, int32_t count) {
    assert(buf != NULL && count > 0);
    char* pos = buf;
+
    while (read(stdin_no, pos, 1) != -1 && (pos - buf) < count) { // 在不出错情况下,直到找到回车符才返回
       switch (*pos) {
        /* 找到回车或换行符后认为键入的命令结束,直接返回 */
@@ -36,9 +37,29 @@ static void readline(char* buf, int32_t count) {
 	    return;
 
 	 case '\b':
-	    if (buf[0] != '\b') {		// 阻止删除非本次输入的信息
+	    if (cmd_line[0] != '\b') {		// 阻止删除非本次输入的信息
 	       --pos;	   // 退回到缓冲区cmd_line中上一个字符
 	       putchar('\b');
+	    }
+	    break;
+
+	 /* ctrl+l 清屏 */
+	 case 'l' - 'a': 
+	    /* 1 先将当前的字符'l'-'a'置为0 */
+	    *pos = 0;
+	    /* 2 再将屏幕清空 */
+	    clear();
+	    /* 3 打印提示符 */
+	    print_prompt();
+	    /* 4 将之前键入的内容再次打印 */
+	    printf("%s", buf);
+	    break;
+
+	 /* ctrl+u 清掉输入 */
+	 case 'u' - 'a':
+	    while (buf != pos) {
+	       putchar('\b');
+	       *(pos--) = 0;
 	    }
 	    break;
 
@@ -64,3 +85,4 @@ void my_shell(void) {
    }
    panic("my_shell: should not be here");
 }
+
