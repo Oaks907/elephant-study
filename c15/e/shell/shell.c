667e2c59d7e33965555ddd2fd0e62d370664ad86
lihaifei <mecuryhai@gmail.com>
2023-10-03 15:17:11 +0800

    c15 e done

--- c15/e/shell/shell.c
+++ c15/e/shell/shell.c
@@ -6,6 +6,7 @@
 #include "global.h"
 #include "assert.h"
 #include "string.h"
+#include "buildin_cmd.h"
 
 #define MAX_ARG_NR 16	   // 加上命令名外,最多支持15个参数
 
@@ -112,11 +113,12 @@ static int32_t cmd_parse(char* cmd_str, char** argv, char token) {
    return argc;
 }
 
-char* argv[MAX_ARG_NR];    // argv必须为全局变量，为了以后exec的程序可访问参数
+char* argv[MAX_ARG_NR];    // argv为全局变量，为了以后exec的程序可访问参数
 int32_t argc = -1;
 /* 简单的shell */
 void my_shell(void) {
    cwd_cache[0] = '/';
+   cwd_cache[1] = 0;
    while (1) {
       print_prompt(); 
       memset(final_path, 0, MAX_PATH_LEN);
@@ -132,12 +134,13 @@ void my_shell(void) {
 	 continue;
       }
       
+      char buf[MAX_PATH_LEN] = {0};
       int32_t arg_idx = 0;
       while(arg_idx < argc) {
-	 printf("%s ", argv[arg_idx]); 
+	 make_clear_abs_path(argv[arg_idx], buf);
+	 printf("%s -> %s\n", argv[arg_idx], buf); 
 	 arg_idx++;
       }
-      printf("\n");
    }
    panic("my_shell: should not be here");
 }
